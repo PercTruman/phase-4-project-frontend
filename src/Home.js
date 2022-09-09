307808389d31ca788d31d21512bd5c5cd0aa9252
PercTruman <drshawnhart@gmail.com>
2022-09-09 10:58:10 -0500

    make fetch route in Home component to get only a teacher's students

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,33 @@
 import React, { useState, useContext, useEffect } from "react";
 import { UserContext } from "./context/UserContext";
 import Navbar from "./Navbar";
-import { NavLink } from "react-router-dom";
 
 const Home = () => {
   const [myStudents, setMyStudents] = useState([]);
   const { user } = useContext(UserContext);
   useEffect(() => {
-    fetch("/students")
+    fetch(`/teachers/${user.id}/students`)
       .then((res) => res.json())
       .then((data) => {
+        console.log(data)
         setMyStudents(
           data.map((s) => (
             <li key={s.id}>
               {s.first_name} {s.last_name}
             </li>
-          )))})})
-      
-    return (
-      <div>
-        <Navbar />
-        <h3>{user.first_name}'s Homepage</h3>
-        <ul>My Students:</ul>
-        {myStudents}
-      </div>
-    );
-  }
+          ))
+        );
+      });
+  },[]);
+
+  return (
+    <div>
+      <Navbar />
+      <h3>{user.first_name}'s Homepage</h3>
+      <ul>My Students:</ul>
+      {myStudents}
+    </div>
+  );
+};
 
 export default Home;
